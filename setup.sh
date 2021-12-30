1b713f7f357bb7b3591c602cd7dada5f8427b839
Steve Bradford <steve@geekprotem.com>
2021-12-30 17:01:35 -0500

    ...

--- setup.sh
+++ setup.sh
@@ -5,12 +5,18 @@ mkdir -p ~/.local/bin
 
 if [ ! -f ~/.local/bin/kubectl ]
 then
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.13/bin/linux/amd64/kubectl
-    chmod +x kubectl
+    curl -LO -o ~/.local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.18.13/bin/linux/amd64/kubectl
+    chmod +x ~/.local/bin/kubectl
 fi
 alias kubectl=~/.local/bin/kubectl
 kubectl version
 
+if [ ! -f ~/.local/bin/get_helm.sh ]
+then
+    curl -fsSL -o ~/.local/bin/get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+done
+if [ ! -f /usr/local/bin/helm ]
+then
+    sh ~/.local/bin/get_helm.sh
+done
 
-# curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-# helm version
